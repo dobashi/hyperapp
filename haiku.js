012f448c05208bca2a281382d5eaca82138972df
dobashi <dobashi@lavans.jp>
2018-01-10 15:17:30 +0900

    delete redundant code

--- haiku.js
+++ haiku.js
@@ -1,22 +1,15 @@
 const { h, app } = hyperapp
 
 const haiku_state = {
-  text1: '1',
-  text2: '2',
-  text3: '3',
   text: 'vert',
 }
 
 const haiku_actions = {
-  text: value =>haiku_state => ({ text1: value[0], text2: value[1], text3: value[2] }),
   vert: value => haiku_state => ({ text: value})
 }
 
 let length = value => value==undefined?0:String(value).length
 let charAt = (value, index) =>value==undefined?'　':length(value)<=index?'　':String(value).charAt(index)
-// function length(value){
-  // return value == undefined?0:String(value).length
-// }
 function convert(value){
   var lines = value.split('\n')
   var max = Math.max(length(lines[0]), length(lines[1]), length(lines[2]))
@@ -24,7 +17,7 @@ function convert(value){
   for(var i=0; i<max; i++){
       result += charAt(lines[2],i) +charAt(lines[1],i) + charAt(lines[0],i) + '\n' 
   }
-  console.log(lines[0])
+  // console.log(lines[0])
   return result
 }
 
@@ -35,7 +28,4 @@ const haiku_view = (state, actions) =>
     h("textarea", { "id": "result", "class":"haiku", "rows":10}, state.text),
   ])
 
-  function haiku(){
-    console.log("haiku()")
-    main = app(haiku_state, haiku_actions, haiku_view, byId("contents"));
-  }
+let haiku = () => windows.main = app(haiku_state, haiku_actions, haiku_view, byId("contents"))
